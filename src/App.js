3cacc207ab73790ab9e6d589b38a463948f47e5e
Araf01 <nevzatarif@gmail.com>
2021-10-07 20:15:12 +0200

    a

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Login from './components/pages/Login';
 import Home from './components/pages/Home';
 import Register from './components/pages/Register';
-import Lockunlock from './js-components/Lockunlock';
+
 
 
 function App() {
@@ -12,10 +12,9 @@ function App() {
     
       <Router>
         <Switch>
-          <Route path='/' exact component={Login} />
-          <Route path='/lockunlock' exact component={Lockunlock} />
-          <Route path='/home' exact component={Home} />
-          <Route path='/register' exact component={Register} />
+          <Route path='/Remoteyourcar' exact component={Login} />
+          <Route path='/Remoteyourcar/home' exact component={Home} />
+          <Route path='/Remoteyourcar/register' exact component={Register} />
 
         </Switch>
       </Router>
